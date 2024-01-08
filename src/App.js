aadad5622aa02507324c649b5b91c3d1ebf67afb
FightWithCode <rahulsharmanoj@gmail.com>
2024-01-08 09:00:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+import { useState } from "react";
+
 function App() {
+  const [step,  setStep] = useState(1)
+  const [counter,  setCounter] = useState(0)
+  var date = new Date()
+  console.log(counter)
+  date.setDate(date.getDate() + counter)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex-display step-container">
+        <button onClick={() => setStep((s) => s - 1)}>-</button>&nbsp;
+        <p>Steps: {step}</p>&nbsp;
+        <button onClick={() => setStep((s) => s + 1)}>+</button> 
+      </div>
+      <div className="flex-display">
+        <button onClick={() => setCounter((c) => c - step)}>-</button>&nbsp;
+        <p>Counter: {counter}</p>&nbsp;
+        <button onClick={() => setCounter((c) => c + step)}>+</button>
+      </div>
+      <div className='flex-display'>
+        <p>{counter === 0 ? "Today is " : counter > 0 ? `${counter} days from today is ` : `${counter} days ago from today is ` }{date.toString()}</p>
+      </div>
     </div>
   );
 }
